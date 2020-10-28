a3db8f20af2a926ee3a7f604e4866f0494b767dc
yogyamodi <yogyamodi@gmail.com>
2020-10-28 13:30:04 +0530

    made stack global and simplified stack operations, shifted todo list to readme and cleaned up lot of code

--- README.md
+++ README.md
@@ -1,4 +1,27 @@
 ## BITS CS F301 Principles of Programming Languages <br> Assignment 1: Language design and type expression computation
 grammar, tokenstream, parsetree, type checking
+todo
+1. Create the char array (with indexes mapped to enum values) (name of array is enumtochar[])
+----------Kartikaya will be making this
+2. createNode(TreeNode* parent, Symbol symbol, int ruleindex);//write in tree.c
+a) crete a node in the tree
+b) different way to creat for S
 
-#aditya e
+3. (DONE) pushRule(struct StackNode* stack, Grammar* G, Symbol symbol, TreeNode* parentlink, int searchfrom)
+    a) returns the index of the rule which is being pushed into stack.
+    b) the rule to be pushed is : LHS is symbol and the searching in the grammar
+    rule starts from index searchfrom
+    c) while pushing into symbols of the rule into stack
+        mention the rule index (of grammar) in stacknode. 
+    d) put parentlink in the node too.
+4. (DONE) popRule(struct StackNode* stack, int ruleindex)
+    a) search for stacknodes from top of the stack, and pop if the topnode has ruleindex 
+5. TreeNode* deleteRule(TreeNode* parent); //write in tree.c
+    a) Deletes the child linkedlistof this parent;
+    b) count number of terminals in the subtree rooted at parent
+    c) go back in the tokenstream by these many
+6. int pushNextRule(struct StackNode* stack, Grammar* G, TreeNode* parentlink, int ruleindex)
+    a)based on parentlink, find the non-terminal and find next rule and push it into the stack
+    b) if no rule exists then get the parent of this node using parentlink and do the same for its parent
+
+7. removeAndReplace (has popRule, deleteRule, and pushnextrule)
--- pushOrPop.c
+++ pushOrPop.c
@@ -1,59 +1,46 @@
 #include "metadata.h"
 
-StackNode* popRule(struct StackNode* stack, int ruleindex){
-	while(stack->ruleindex == ruleindex){
-####		stack = pop(stack);
-	}
-	return stack;
+void popRule(int ruleno){
+	while(stack->ruleno == ruleno)
+		pop();
 }
 
-int pushRule(struct StackNode* stack, Grammar* G, TreeNode* parentlink, Symbol symbol, int searchfrom){
+int pushRule(Grammar* G, TreeNode* parent, Symbol symbol, int searchfrom){
 
-	for(int i = searchfrom; i<GRAMMAR_SIZE; i++){
+	for(int i = searchfrom; i < GRAMMAR_SIZE; i++){
 		if(G[i].symbol == symbol){
-			parentlink->child = makeTreeNodelist(G[i],i,parentlink);
-####			newhead = pushReverseGrammarRule(G[i]->next,i,parentlink,stack);
+			parent->child = makeTreeNodelist(G[i], i, parent);
+			pushReverseGrammarRule(G[i] -> next, i, parent);
 			return i;
 		}
 	}
 	return -1;
 }
 
-StackNode* pushReverseGrammarRule(Grammar* head, int ruleno, TreeNode* parent, StackNode* top){
-	if(head->next!=NULL){
-		top = pushReverseGrammarRule(head->next,ruleno,parent,top);
-	}
-
-	//this section will be first executed by the last node in the Grammar rule and it will 
-	//return the new top of the stack after pushing equivalent stackNode
-	StackNode* this;
-	this = (StackNode*)malloc(sizeof(Grammar));
-	this->data = head->symbol;
-####	this->is_term = 
-	this->parenttree = parent;
-	this->next = top;
-	this->ruleindex = ruleno;
-	top = this;
-	return top; 
+void pushReverseGrammarRule(Grammar* head, int ruleno, TreeNode* parent){
+	if(head->next!=NULL)
+		pushReverseGrammarRule(head -> next, ruleno, parent);
+	push(head -> is_term, head -> symbol, ruleno, parent);
+	return;
 }
 
 TreeNode* makeTreeNodelist(Grammar* head, int ruleno, TreeNode* parent){
-	TreeNode* children,temp,prev_sib;
-	bool first=true;
-	while(head!=NULL){
+	TreeNode* children, temp, prev_sib;
+	bool first = true;
+	while(head != NULL){
 		temp = (TreeNode*)malloc(sizeof(TreeNode));
 		if(first){
 			children = temp;
 			first = false;
 		}
-		temp->ruleindex = ruleno;
-		temp->symbol = head->symbol;
-		temp->parent = parent;
-		temp->nextsib = NULL;
-		temp->child = NULL;
-		prev_sib->nextsib = temp;
+		temp -> ruleno = ruleno;
+		temp -> symbol = head -> symbol;
+		temp -> parent = parent;
+		temp -> nextsib = NULL;
+		temp -> child = NULL;
+		prev_sib -> nextsib = temp;
 		prev_sib = temp;
-		head= head->next;
+		head = head -> next;
 	}
 	return children;
 }
