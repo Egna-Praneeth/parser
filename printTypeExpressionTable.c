0ee319716a75644a43125f799e40ab7a0ca71454
kartikayasharma <kartikayasharma@DESKTOP-58BF2KV.localdomain>
2020-10-29 21:50:09 +0530

    kartikaya

---
+++ printTypeExpressionTable.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include "metadata.h"
+#include "typeExpr.h"
+int main()
+{
+    TypeExprNode* ptr;
+    
+    while (ptr)
+    {
+        Array a;
+        printf("<%s> <%s> <%s> ", ptr->name, prim_or_arr_str[ptr->tag1],stat_or_dyn_str[ptr->tag2]);
+        switch (ptr->tag1)
+        {
+        case 0:
+            printf(" <type = %s>", enumtochar[ptr->TE.primitive]);
+            break;
+        case 1:
+            a = ptr->TE.arr;
+            printf(" <type = %s, dimensions = %d", prim_or_arr_str[ptr->tag1], a.dimensions);
+            for (int i = 0; i < a.dimensions; i++)
+            {
+                RangeRect* r = a.rng.rect;
+                printf("range_R%d = (%d, %d), ", i+1, r->lower.limit, r->upper.limit);
+                r = r->next;
+            }
+            printf("basicElemntType = %s>", enumtochar[a.t]);
+            break;
+        case 2:
+            a = ptr->TE.arr;
+            printf(" <type = %s, dimensions = %d, range_R1 = (%d, %d), ", 
+            prim_or_arr_str[ptr->tag1], a.dimensions, a.rng.jagged[0]->data, a.rng.jagged[0]->next->data);
+            
+            RangeJagged* r = a.rng.jagged[1];
+            printf("range_R2 = ( ");
+            while (r->next)
+            {
+                printf("%d", r->data);
+                if (a.dimensions==3)
+                {
+                    printf("[");
+                    RangeJagged3D* r1 = r->head;
+                    while (r1->next)
+                    {
+                        printf("%d, ", r1->data);
+                        r1 = r1->next;
+                    }
+                    printf("%d]",r1->data);  
+                }
+                printf(", ");
+                r = r->next;
+            }
+            printf("%d), ", r->data);
+            printf("basicElemntType = %s>", enumtochar[a.t]);
+            break;
+        }
+    
+        ptr = ptr->next;
+    }
+}
---
+++ typeExpr.h
@@ -0,0 +1,79 @@
+enum type{
+	Integer,
+	Bool,
+	Real
+};typedef union bound{
+	int limit;
+	char* var;
+}Bound;
+
+typedef struct range_rect{
+		Bound lower,upper;
+		struct range_rect *next;
+}RangeRect;
+
+typedef struct range_jagged{
+		int data;
+		struct range_jagged *next;
+		struct range_jagged3D *head;
+
+}RangeJagged;
+
+typedef union range_type{
+	RangeRect* rect;
+	RangeJagged* jagged[2];
+}RangeType;
+
+typedef struct array{
+	enum type t;
+	int dimensions;
+ 	RangeType rng;
+
+}Array;
+
+
+
+typedef struct range_jagged3D{
+	int data;
+	struct range_jagged3D *next;
+}RangeJagged3D;
+
+
+
+
+
+
+typedef union typeExpression{
+	Array arr;
+	enum type primitive;
+}TypeExpression;
+
+enum prim_or_arr{
+	primitive,
+	rect_array,
+	jagged_array
+};
+char* prim_or_arr_str[] = { 
+	"primitive",
+	"rect_array",
+	"jagged_array"
+};
+char* stat_or_dyn_str[] = { 
+	"Static",
+	"Dynamic",
+	"NA"
+};
+
+enum stat_or_dyn{
+	Static,
+	Dynamic,
+	NA
+};
+
+typedef struct typeExprNode{
+	char* name;
+	enum prim_or_arr tag1;
+	enum stat_or_dyn tag2;
+	TypeExpression TE;
+	struct typeExprNode *next;
+}TypeExprNode;
